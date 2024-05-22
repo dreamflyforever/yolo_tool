a2483441d291bca8fa6ca108fb4933a7eab6ceff
dreamflyforever <1150660744@qq.com>
2024-05-22 11:50:23 +0800

    add label
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- yolo_txt2verify.py
+++ yolo_txt2verify.py
@@ -2,7 +2,7 @@
 import os
 import cv2
 
-path = "."         # jpeg图片和对应的生成结果的txt标注文件，放在一起
+path = "/Users/jim/workspace/sleepy/rv1103/easyai/display/seven/verity"         # jpg图片和对应的生成结果的txt标注文件，放在一起
 path3 = "abc"    # 裁剪出来的小图保存的根目录
 w = 640                         # 原始图片resize
 h = 640
@@ -12,7 +12,7 @@ txt_total = []
 file = os.listdir(path)
 for filename in file:
     first,last = os.path.splitext(filename)
-    if last == ".jpeg":                      # 图片的后缀名
+    if last == ".jpg":                      # 图片的后缀名
         img_total.append(first)
     #print(img_total)
     else:
@@ -20,7 +20,7 @@ for filename in file:
 
 for img_ in img_total:
     if img_ in txt_total:
-        filename_img = img_+".jpeg"          # 图片的后缀名
+        filename_img = img_+".jpg"          # 图片的后缀名
         # print('filename_img:', filename_img)
         path1 = os.path.join(path,filename_img)
         img = cv2.imread(path1)
@@ -39,10 +39,11 @@ for img_ in img_total:
                 lefttopy = int(y_center-height/2.0)
                 roi = img[lefttopy+1:lefttopy+height+3,lefttopx+1:lefttopx+width+1]   # [左上y:右下y,左上x:右下x] (y1:y2,x1:x2)需要调参，否则裁剪出来的小图可能不太好
                 print('roi:', roi)                        # 如果不resize图片统一大小，可能会得到有的roi为[]导致报错         
-                filename_last = img_+"_"+str(n)+".jpeg"    # 裁剪出来的小图文件名
+                filename_last = img_+"_"+str(n)+".jpg"    # 裁剪出来的小图文件名
                 # print(filename_last)
                 path2 = os.path.join(path3,"roi")           # 需要在path3路径下创建一个roi文件夹
                 print('path2:', path2)                    # 裁剪小图的保存位置
+                cv2.putText(roi, aa[0], (100, 100), cv2.FONT_HERSHEY_COMPLEX, 2.0, (100, 200, 200), 5)
                 cv2.imwrite(os.path.join(path2,filename_last),roi)
                 n = n+1
     else:
